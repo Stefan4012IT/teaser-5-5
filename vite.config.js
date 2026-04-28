5e2c42e36d2b40178fb974a5602a4d71f801720a
stefan4012it <stefanra555@gmail.com>
2026-04-28 11:04:06 +0200

    Prepare separate GitHub Pages and production builds

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/teaser-5-5/',
-})
+  base: mode === 'github' ? '/teaser-5-5/' : '/',
+}))
